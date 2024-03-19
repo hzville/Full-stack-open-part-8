6ac9905777aeec1ec85c56dc998d1c70baaf7c3b
Ville Halonen <71012693+hzville@users.noreply.github.com>
2024-03-19 21:12:26 +0200

    8.3

--- server.js
+++ server.js
@@ -12,11 +12,19 @@ const typeDefs = `
     genres: [String!]
   }
 
+  type Author {
+    name: String!
+    born: Int
+    id: ID!
+    bookCount: Int
+  }
+
 
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -24,7 +32,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => books.filter((book) => book.author == root.name).length
   }
 }
 
