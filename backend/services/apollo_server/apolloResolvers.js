0afe2fa635d714c1353eddf66aee9ca8ce0a79e9
Ville Halonen <71012693+hzville@users.noreply.github.com>
2024-09-05 00:07:48 +0300

    8.15

--- backend/services/apollo_server/apolloResolvers.js
+++ backend/services/apollo_server/apolloResolvers.js
@@ -1,5 +1,6 @@
 const Author = require('../../models/authorSchema')
 const Book = require('../../models/bookSchema')
+const { GraphQLError } = require('graphql')
 
 const resolvers = {
   Query: {
@@ -22,12 +23,22 @@ const resolvers = {
     addBook: async (root, args) => {
       const book = new Book({...args})
       const authorPresent = await Author.findOne({ name: args.author })
-      if (authorPresent != null) {
-        book.author = authorPresent
-        return await book.save()
-      } else {
-        book.author = await new Author({name: args.author}).save()
-        return await book.save()
+      try {
+        if (authorPresent != null) {
+          book.author = authorPresent
+          return await book.save()
+        } else {
+          book.author = await new Author({name: args.author}).save()
+          return await book.save()
+        }
+      } catch (error) {
+        throw new GraphQLError('Adding new book failed',{
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: [args.title, args.author],
+            error
+          }
+        })
       }
     },
     editAuthor: async (root, args) => {
@@ -36,7 +47,17 @@ const resolvers = {
         return null
       }
       author.born = args.setBornTo
-      return await author.save()
+      try {
+        return await author.save()
+      } catch (error){
+        throw new GraphQLError('Editing author failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error
+          }
+        })
+      }
     }
   }
 }
