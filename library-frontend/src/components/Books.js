5d1337ca156b1a826bf902f685430588436abb51
Ville Halonen <71012693+hzville@users.noreply.github.com>
2024-09-05 14:26:34 +0300

    8.17

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -24,11 +24,11 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.data.allBooks.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
+          {books.data.allBooks.map((book) => (
+            <tr key={book.title}>
+              <td>{book.title}</td>
+              <td>{book.author.name}</td>
+              <td>{book.published}</td>
             </tr>
           ))}
         </tbody>
